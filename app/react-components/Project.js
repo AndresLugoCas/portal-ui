b11d13793f7f151150ded56743e7b6799287cad8
Jeffrey Burt <jeffrey.burt@oicr.on.ca>
2016-11-22 16:53:32 -0500

    :sparkle: [PRTL-371] support list

--- app/react-components/Project.js
+++ app/react-components/Project.js
@@ -197,8 +197,8 @@ const Project = ({
             thToTd={[
               { th: 'Project ID', td: project.project_id },
               { th: 'Project Name', td: project.name },
-              { th: 'Disease Type', td: project.disease_type },
-              { th: 'Primary Site', td: project.primary_site },
+              { th: `Disease Type${project.disease_type.length > 1 ? 's' : ''}`, td: project.disease_type.map(p => <div>{p}</div>) },
+              { th: `Primary Site${project.primary_site.length > 1 ? 's' : ''}`, td: project.primary_site.map(p => <div>{p}</div>) },
               { th: 'Program', td: project.program.name },
             ]}
           />
